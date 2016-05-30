2fdfafb7a5ec60a58ad46c0d017fb8742d60915d
Vikas <vikas.singh093@gmail.com>
2016-05-31 00:35:44 +0530

    Add program is done

--- addparent.py
+++ addparent.py
@@ -1,3 +1,12 @@
 class add:
+  def __init__(self):
+    print "Two number which you want"
+   
+  def addnumbers(self):
+    firstnumber= int(raw_input("first"))
+    secondnumber= int(raw_input("second"))
+    Sum =  firstnumber + secondnumber
+    print "Here's your result" ,Sum
 
-
+add1 =  add()
+add1.addnumbers()
