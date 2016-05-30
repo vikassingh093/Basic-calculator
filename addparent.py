a14eafa02c6e0ba086ae24baa083d1803c4b3fb3
Vikas <vikas.singh093@gmail.com>
2016-05-31 01:11:08 +0530

    changes in add

--- addparent.py
+++ addparent.py
@@ -1,12 +1,17 @@
 class add:
+  firstnumber = 0
+  secondnumber = 0
   def __init__(self):
     print "Two number which you want"
    
   def addnumbers(self):
-    firstnumber= int(raw_input("first"))
-    secondnumber= int(raw_input("second"))
-    Sum =  firstnumber + secondnumber
+    add.firstnumber= int(raw_input("first"))
+    add.secondnumber= int(raw_input("second"))
+
+  def result (self):
+    Sum =  add.firstnumber + add.secondnumber
     print "Here's your result" ,Sum
 
 add1 =  add()
 add1.addnumbers()
+add1.result()
